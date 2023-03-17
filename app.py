bdabc749deddd34fc2ffc923dfc15dc51e3616aa
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 21:12:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def page1():
         else:
             image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
             st.image(image, use_column_width=True)
+            
+    if st.button("Next"):
+        st.session_state.current_page = "Page 1"
 
 def page2():
     st.title("Page 2")
