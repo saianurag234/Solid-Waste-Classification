659dc86e5ebafd99ce4748e845252f86471439ae
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 20:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import streamlit as st
-import cv2
-import numpy as np
 
-st.title("Solid Waste Classfication")
-camera_image = st.sidebar.camera_input("Take a Photo of the waste")
-st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
-upload_file = st.sidebar.file_uploader("Upload the photo of the Waste")
+def page1():
+    st.title("Page 1")
+    # Add content for page 1 here
+    st.write("Welcome to Page 1!")
+    if st.button("Next"):
+        st.session_state.current_page = "Page 2"
 
-button = st.button("Predict")
+def page2():
+    st.title("Page 2")
+    # Add content for page 2 here
+    st.write("Welcome to Page 2!")
+    if st.button("Next"):
+        st.session_state.current_page = "Page 3"
 
-if button:
-  if camera_image is not None:
-    image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
-    st.image(image, use_column_width=True)
-  else:
-    image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
-    st.image(image, use_column_width=True)
-     
+def page3():
+    st.title("Page 3")
+    # Add content for page 3 here
+    st.write("Welcome to Page 3!")
+    if st.button("Next"):
+        st.session_state.current_page = "Page 1"
 
-st.session_state.page_select = st.button('Next')
+# Create a dictionary of page names and their corresponding functions
+pages = {
+    "Page 1": page1,
+    "Page 2": page2,
+    "Page 3": page3
+}
 
-if st.session_state.page_select == True:
-    st.title('Page 1')
-    next = st.button('Go to page 2')
-    if next:
-        st.session_state.page_select = 'Page 2'
+# Set initial page to display
+if "current_page" not in st.session_state:
+    st.session_state.current_page = "Page 1"
+
+# Call the selected page function
+pages[st.session_state.current_page]()
