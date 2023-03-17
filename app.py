a9dad4fd8e370d23e68c25bb9ca562585487c28a
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 18:38:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import numpy as np
 st.title("Solid Waste Classfication")
 camera_image = st.sidebar.camera_input("Take a Photo of the waste")
 st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
-upload_image = st.sidebar.file_uploader("Upload the photo of the Waste")
+upload_file = st.sidebar.file_uploader("Upload the photo of the Waste")
 
 if camera_image is not None:
   image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
 else:
-  image = cv2.imdecode(np.frombuffer(upload_image.read(), np.uint8), 1)
+  image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
 
-st.image(image)
+st.image(image, use_column_width=True)
