eefadd0d1520019233131a20663c1ce675e1ff7e
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 20:23:58 +0530

    Update app.py

--- my_app/app.py
+++ my_app/app.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+st.title("Solid Waste Classfication")
+camera_image = st.sidebar.camera_input("Take a Photo of the waste")
+st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
+upload_file = st.sidebar.file_uploader("Upload the photo of the Waste")
+
+button = st.button("Predict")
+
+if button:
+  if camera_image is not None:
+    image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
+    st.image(image, use_column_width=True)
+  else:
+    image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
+    st.image(image, use_column_width=True)
+     
+
+st.session_state.page_select = st.button('Next')
+
+if st.session_state.page_select == True:
+    st.title('Page 1')
+    next = st.button('Go to page 2')
+    if next:
+        st.session_state.page_select = 'Page 2'
 
