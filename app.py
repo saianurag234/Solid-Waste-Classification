dd099264021d89d64445d5fff446a255c2e79ae5
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 22:22:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,21 @@
 import streamlit as st
-import cv2
-import numpy as np
 
+def main():
+    # Set page title
+    st.set_page_config(page_title="Image Predictor")
 
-camera_image = st.sidebar.camera_input("Take a Photo of the waste")
-st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
-upload_file = st.sidebar.file_uploader("Upload the photo of the Waste")
+    # Set page heading
+    st.header("Image Predictor")
 
-if camera_image is not None:
-    image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
-            
-if upload_file is not None:
-    image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
-        
-if camera_image is not None and upload_file is not None:
-    st.subheader("Please upload an image")
+    # Show image
+    image = st.file_uploader("Upload an image")
+    if image is not None:
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-def page1():
-    st.title("Solid Waste Classfication")
-    st.image("https://millerrecycling.com/wp-content/uploads/2020/10/organic-waste.jpg", width=None)
-            
-    if st.button("Next", key="next_page"):
-        st.session_state.current_page = "Page 2"
+    # Add "Predict" button
+    if st.button("Predict"):
+        # Navigate to new page
+        st.experimental_rerun()
 
-def page2():
-    st.image(image, use_column_width=True)
-    if st.button("Back", key="prev_page"):
-        st.session_state.current_page = "Page 1"
-
-
-# Create a dictionary of page names and their corresponding functions
-pages = {
-    "Page 1": page1,
-    "Page 2": page2
-}
-
-# Set initial page to display
-if "current_page" not in st.session_state:
-    st.session_state.current_page = "Page 1"
-
-# Call the selected page function
-pages[st.session_state.current_page]()
+if __name__ == '__main__':
+    main()
