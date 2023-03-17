1b27032a718ec190c01f6185803308ca351443f5
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 18:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Solid Waste Classfication")
-st.sidebar.markdown("<h3 style='text-align: center;'>Take a Photo of the waste </h3>", unsafe_allow_html=True)
+st.sidebar.markdown("<b style='text-align: left;'>Take a Photo of the waste </b>", unsafe_allow_html=True)
 st.sidebar.camera_input("")
 st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
 st.sidebar.file_uploader("Upload the photo of the Waste")
