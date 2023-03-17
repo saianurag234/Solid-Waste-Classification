dc332075389cb95c53de1c59dddd08cd8b146e76
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 18:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,7 @@ import streamlit as st
 
 st.title("Solid Waste Classfication")
 st.sidebar.camera_input("Take a Photo of the waste")
-title_alignment=
-"""
-<style>
-#the-title {
-  text-align: center
-}
-</style>
-"""
+
 st.markdown(title_alignment, unsafe_allow_html=True)
 
 st.sidebar.file_uploader("Upload the photo of the Waste")
