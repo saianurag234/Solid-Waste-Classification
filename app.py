c7c5a2392d6c6357ae07f20eb6634ca7a03290ed
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 22:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,18 @@ camera_image = st.sidebar.camera_input("Take a Photo of the waste")
 st.sidebar.markdown("<h3 style='text-align: center;'>Or </h3>", unsafe_allow_html=True)
 upload_file = st.sidebar.file_uploader("Upload the photo of the Waste")
 
+if camera_image is not None:
+    image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
+            
+if upload_file is not None:
+    image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
+        
+if camera_image is not None and upload_file is not None:
+    st.subheader("Please upload an image")
+
 def page1():
     st.title("Solid Waste Classfication")
     st.image("https://millerrecycling.com/wp-content/uploads/2020/10/organic-waste.jpg", width=None)
-    
-    if camera_image is not None:
-        image = cv2.imdecode(np.frombuffer(camera_image.read(), np.uint8), 1)
-            
-    if upload_file is not None:
-        image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
-        
-    if camera_image is not None and upload_file is not None:
-        st.subheader("Please upload an image")
-    
             
     if st.button("Next"):
         st.session_state.current_page = "Page 2"
