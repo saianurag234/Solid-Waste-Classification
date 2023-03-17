a1fddf5c3e78d5c055da0503eb75f3aa6a8e7242
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-17 18:13:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("Solid Waste Classfication")
 st.sidebar.camera_input("Take a Photo of the waste")
+st.sidebar.subheader("Or")
+st.sidebar.file_uploader("Upload the photo of the Waste")
