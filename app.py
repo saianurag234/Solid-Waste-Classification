f722383aefdf231d125c6f1dda463528b25f65da
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 08:46:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ import streamlit as st
 
 # Define a list of pages
 pages = ["Page 1", "Page 2", "Page 3"]
-page_index = 0
+current_page_index = 0
 
 # Define a function to handle the "Next" button click
 def next_page():
-    global page_index
-    page_index += 1
-    if page_index >= len(pages):
-        page_index = 0
-    return pages[page_index]
+    global current_page_index
+    current_page_index = (current_page_index + 1) % len(pages)
 
-# Create a Streamlit app
+# Create the Streamlit app
 def main():
     st.title("Next Button Navigation")
-    page = next_page()
-    st.write(f"You are on {page}")
-    st.button("Next", on_click=next_page)
+
+    # Display the current page
+    st.write(f"You are on {pages[current_page_index]}")
+
+    # Add a "Next" button to navigate to the next page
+    if st.button("Next"):
+        next_page()
 
 if __name__ == "__main__":
     main()
