3c2b255c307125ff1d9c8a2cde2094e2471673a3
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 08:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import streamlit as st
 
-# Define a list of pages
-pages = ["Page 1", "Page 2", "Page 3"]
-current_page_index = 0
-
-# Define a function to handle the "Next" button click
-def next_page():
-    global current_page_index
-    current_page_index = (current_page_index + 1) % len(pages)
+# Define a dictionary of page names and their contents
+pages = {
+    "Page 1": "This is page 1",
+    "Page 2": "This is page 2",
+    "Page 3": "This is page 3"
+}
 
 # Create the Streamlit app
 def main():
-    st.title("Next Button Navigation")
+    st.title("Single Click Button Navigation")
 
-    # Display the current page
-    st.write(f"You are on {pages[current_page_index]}")
+    # Create a list of page names
+    page_names = list(pages.keys())
 
-    # Add a "Next" button to navigate to the next page
-    if st.button("Next"):
-        next_page()
+    # Display a dropdown to select the current page
+    current_page = st.selectbox("Select a page", page_names)
+
+    # Display the contents of the current page
+    st.write(pages[current_page])
+
+    # Create buttons to navigate to the previous and next pages
+    current_page_index = page_names.index(current_page)
+    if current_page_index > 0:
+        if st.button("Previous"):
+            current_page = page_names[current_page_index - 1]
+    if current_page_index < len(page_names) - 1:
+        if st.button("Next"):
+            current_page = page_names[current_page_index + 1]
 
 if __name__ == "__main__":
     main()
