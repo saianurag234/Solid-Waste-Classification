a84a51639493f1289da0a52735117265ebf7c570
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 10:50:43 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
-want_to_contribute = st.button("About")
+st.title("Solid Waste Classification")
+want_to_contribute = st.button("Click Here to upload the Image")
 if want_to_contribute:
-    switch_page("About")
+    switch_page("📸_Upload Image")
