fff46ceab8a29dee0865b36886567209cc4bf36a
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 11:24:05 +0530

    Update 🙋_Contribution.py

--- pages/🙋_Contribution.py
+++ pages/🙋_Contribution.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("🙋 Contributions")
+st.markdown("<h3 style='text-align: center;;'>P.S.S.Sai Keerthana</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;;'>P.Komal Sai Anurag</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;;'>Udayagiri Varun</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;;'>Sejal Singh</h3>", unsafe_allow_html=True)
