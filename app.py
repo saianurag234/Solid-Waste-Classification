656cd1c0bf109ceb7feef6133a88f46806399484
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 08:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,23 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
-EXAMPLE_NO = 1
+# Define a list of pages
+pages = ["Page 1", "Page 2", "Page 3"]
+page_index = 0
 
+# Define a function to handle the "Next" button click
+def next_page():
+    global page_index
+    page_index += 1
+    if page_index >= len(pages):
+        page_index = 0
+    return pages[page_index]
 
-def streamlit_menu(example=1):
-    if example == 1:
-        # 1. as sidebar menu
-        with st.sidebar:
-            selected = option_menu(
-                menu_title="Main Menu",  # required
-                options=["Home", "Projects", "Contact"],  # required
-                icons=["house", "book", "envelope"],  # optional
-                menu_icon="cast",  # optional
-                default_index=0,  # optional
-            )
-        return selected
+# Create a Streamlit app
+def main():
+    st.title("Next Button Navigation")
+    page = next_page()
+    st.write(f"You are on {page}")
+    st.button("Next", on_click=next_page)
 
-    if example == 2:
-        # 2. horizontal menu w/o custom style
-        selected = option_menu(
-            menu_title=None,  # required
-            options=["Home", "Projects", "Contact"],  # required
-            icons=["house", "book", "envelope"],  # optional
-            menu_icon="cast",  # optional
-            default_index=0,  # optional
-            orientation="horizontal",
-        )
-        return selected
-
-    if example == 3:
-        # 2. horizontal menu with custom style
-        selected = option_menu(
-            menu_title=None,  # required
-            options=["Home", "Projects", "Contact"],  # required
-            icons=["house", "book", "envelope"],  # optional
-            menu_icon="cast",  # optional
-            default_index=0,  # optional
-            orientation="horizontal",
-            styles={
-                "container": {"padding": "0!important", "background-color": "#fafafa"},
-                "icon": {"color": "orange", "font-size": "25px"},
-                "nav-link": {
-                    "font-size": "25px",
-                    "text-align": "left",
-                    "margin": "0px",
-                    "--hover-color": "#eee",
-                },
-                "nav-link-selected": {"background-color": "green"},
-            },
-        )
-        return selected
-
-
-selected = streamlit_menu(example=EXAMPLE_NO)
-
-if selected == "Home":
-    st.title(f"You have selected {selected}")
-if selected == "Projects":
-    st.title(f"You have selected {selected}")
-if selected == "Contact":
-    st.title(f"You have selected {selected}")
+if __name__ == "__main__":
+    main()
