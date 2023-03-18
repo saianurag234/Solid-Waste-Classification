a7a98b8e9058420e32fa3f2d2505f06dd405eb26
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 10:56:43 +0530

    Update 📸_Upload Image.py

--- pages/📸_Upload Image.py
+++ pages/📸_Upload Image.py
@@ -1,16 +1,20 @@
 import streamlit as st
 
-st.subheader("Upload the Image")
-upload_file = st.file_uploader(" ")
-
-st.markdown("<h2 style='text-align: center;;'>Or</h2>", unsafe_allow_html=True)
-
 st.subheader("Take a photo")
 camera_input = st.camera_input(" ")
 
-if upload_file is not None:
-  image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
-elif camera_input is not None:
-  image = cv2.imdecode(np.frombuffer(camera_input.read(), np.uint8), 1)
-else:
-  st.write("Please upload the image")
+st.markdown("<h2 style='text-align: center;;'>Or</h2>", unsafe_allow_html=True)
+
+st.subheader("Upload the Image")
+upload_file = st.file_uploader(" ")
+
+Predict = st.button("Predict")
+
+if Predict:
+  if upload_file is not None:
+    image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
+  elif camera_input is not None:
+    image = cv2.imdecode(np.frombuffer(camera_input.read(), np.uint8), 1)
+  else:
+    st.write("Please upload the image")
+   switch_page("Contribution")
