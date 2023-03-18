06da1745c8426ee820e6f6836b2f5981b6ba72c5
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-18 10:33:28 +0530

    Update 📸_Upload Image.py

--- pages/📸_Upload Image.py
+++ pages/📸_Upload Image.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+upload_file = st.file_uploader("Upload the Image")
+camera_input = st.camera_input("Take a photo")
+
+if upload_file is not None:
+  image = cv2.imdecode(np.frombuffer(upload_file.read(), np.uint8), 1)
+elif camera_input is not None:
+  image = cv2.imdecode(np.frombuffer(camera_input.read(), np.uint8), 1)
+else:
+  st.write("Please upload the image")
